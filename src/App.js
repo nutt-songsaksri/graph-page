9ae3f4976a2fb47990b5a2b82a703e884f7544eb
DESKTOP-7I51HR0\Nuttyld <nutt_songsaksri@hotmail.com>
2021-02-24 03:49:04 +0700

    separate the Legend component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ import { Box, Grid, Card, CardContent } from "@material-ui/core";
 import Topbar from "./components/Topbar/Topbar";
 import Viewmore from "./components/Viewmore/Viewmore";
 import Legend from "./components/Legend/Legend";
+import { Doughnut } from "react-chartjs-2";
 
 function App() {
+  // const data = {
+  //   labels: ["Red", "Green", "Yellow"],
+  //   datasets: [
+  //     {
+  //       data: [300, 50, 100],
+  //       backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+  //       hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+  //     },
+  //   ],
+  // };
   return (
     <Box style={{ height: "100vh", backgroundColor: "#d1d1d1" }}>
       <Grid
@@ -16,16 +27,32 @@ function App() {
         style={{ height: "100%" }}
       >
         <Grid item>
-          <Card style={{ width: 716 }}>
+          <Card style={{ width: 716, height: 364 }}>
             <CardContent>
-              <Grid container xs={12} direction="column">
-                <Grid item xs={12}>
+              <Grid container direction="column">
+                <Grid item>
                   <Topbar />
                 </Grid>
-                <Grid item xs={12}>
-                  <Legend color={"blue"} />
-                  <Legend color={"red"} />
+                {/* 29.3 */}
+                <Grid item style={{ marginTop: 26.12, marginLeft: 11 }}>
+                  <Legend color={"#2F69CC"} />
+                  {/* top={"26.12"} left={"11"} */}
                 </Grid>
+                <Grid item style={{ marginTop: 29.3, marginLeft: 11 }}>
+                  <Legend color={"#129219"} />
+                </Grid>
+                <Grid item style={{ marginTop: 29.3, marginLeft: 11 }}>
+                  <Legend color={"#F99A02"} />
+                </Grid>
+                <Grid item style={{ marginTop: 29.3, marginLeft: 11 }}>
+                  <Legend color={"#DA3812"} />
+                </Grid>
+                <Grid item style={{ marginTop: 29.3, marginLeft: 11 }}>
+                  <Legend color={"#119219"} />
+                </Grid>
+                {/* <Grid item>
+                  <Doughnut data={data} width={100} />
+                </Grid> */}
                 <Grid item xs={12}>
                   <Viewmore />
                 </Grid>
--- src/components/Legend/Legend.jsx
+++ src/components/Legend/Legend.jsx
@@ -1,92 +1,68 @@
 import React from "react";
 import { Grid, Typography } from "@material-ui/core";
+import classes from "./Legend.module.css";
 
 const Legend = (props) => {
   return (
     <Grid
-      item
       container
-      direction="row"
-      xs={4}
-      style={{
-        backgroundColor: "rebeccapurple",
-        paddingTop: "11px",
-      }}
+      // style={{
+      //   marginTop: "{props.top}",
+      //   marginLeft: "{props.left}",
+      // }}
     >
       <Grid
         item
         container
         style={{
-          backgroundColor: "crimson",
+          display: "block",
+          width: 152,
+          height: 21,
+        }}
+      >
+        <Grid item>
+          <Typography className={classes.Option}>Option</Typography>
+        </Grid>
+      </Grid>
+
+      <Grid
+        item
+        container
+        direction="column"
+        style={{
+          display: "block",
+          width: 36.37,
+          height: 25.7,
+          marginLeft: 8.81,
         }}
       >
         <Grid
           item
-          container
-          justify="flex-end"
-          alignItems="flex-start"
-          xs={6}
           style={{
-            backgroundColor: "orange",
+            backgroundColor: "#000000",
           }}
         >
-          <Typography
-            style={{
-              fontSize: 14,
-              fontWeight: 600,
-              fontFamily: "Poppins,sans-serif",
-              fontStyle: "normal",
-              color: "#343A40",
-              display: "block",
-            }}
-          >
-            Option
-          </Typography>
+          <Typography className={classes.Percentage}>100%</Typography>
         </Grid>
-
-        <Grid item container direction="column" alignItems="center" xs={6}>
-          <Grid
-            item
-            style={{
-              backgroundColor: "#000000",
-              display: "block",
-            }}
-          >
-            <Typography
-              style={{
-                fontSize: 12,
-                fontWeight: "bold",
-                fontFamily: "Poppins,sans-serif",
-                fontStyle: "normal",
-                color: "#FFFFFF",
-                display: "block",
-                marginLeft: 1,
-                marginRight: 5,
-                height: "16.456px",
-              }}
+        <Grid
+          item
+          container
+          style={{
+            marginLeft: 22,
+          }}
+        >
+          <Grid item>
+            <svg
+              width="14.45px"
+              height="11.6px"
+              viewBox="0 0 15 12"
+              xmlns="http://www.w3.org/2000/svg"
+              style={{ display: "block", marginTop: -0.325 }}
             >
-              100%
-            </Typography>
-          </Grid>
-          <Grid
-            item
-            style={{
-              marginLeft: 30.59,
-              height: "16.456px",
-            }}
-          >
-            <Grid item style={{ marginTop: "-6.19px" }}>
-              <svg
-                width="14.45px"
-                height="11.6px"
-                viewBox="0 0 15 12"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g fill={props.color}>
-                  <path d="M5.03055 0.212402C4.75703 2.7744 3.08739 5.5856 0.0187988 8.6432L3.93076 11.8128C9.50951 8.3688 13.014 4.5272 14.4443 0.288003C14.4529 0.262803 14.4614 0.237602 14.47 0.212402H5.03055Z" />
-                </g>
-              </svg>
-            </Grid>
+              <g fill={props.color}>
+                <path d="M5.03055 0.212402C4.75703 2.7744 3.08739 5.5856 0.0187988 8.6432L3.93076 11.8128C9.50951 8.3688 13.014 4.5272 14.4443 0.288003C14.4529 0.262803 14.4614 0.237602 14.47 0.212402H5.03055Z" />
+              </g>
+            </svg>
           </Grid>
         </Grid>
       </Grid>
